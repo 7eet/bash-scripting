8a9dd745a439bceef24cad44538a5f48daa957d8
7eet <raisimranjeetsingh@gmail.com>
2019-11-07 19:19:04 +0530

    case statement

---
+++ case.sh
@@ -0,0 +1,20 @@
+echo "CASE STATEMENT"
+
+vi="hii"
+
+case $vi in 
+	"Hello" ) echo "Hello :)" ;;
+	"Hii" ) echo "HIII :)" ;;
+	* ) echo "hii" ;;
+esac
+
+
+
+numb=25
+case $numb in 
+	5  ) echo "  5 " ;;
+	2 ) echo "  2 " ;;
+	* ) echo "0" ;;
+esac
+
+echo "BYE"
